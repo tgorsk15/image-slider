48323d7081e054f3c8cac711422e84bb1d2dc030
tgorsk15 <tgbusiness463@gmail.com>
2024-01-22 10:46:04 -0600

    Build beginning structure of buttonLogic.js

---
+++ src/buttonLogic.js
@@ -0,0 +1,23 @@
+import { slidesArray } from "./index";
+
+
+
+// eslint-disable-next-line import/prefer-default-export
+export const arrowsController = function () {
+
+    function nextArrow() {
+
+    }
+
+
+    function backArrow() {
+
+
+    }
+
+
+    return {nextArrow, backArrow}
+}
+
+
+
