c867520a6308d79ad6e00d9541334d358811c2d3
tgorsk15 <tgbusiness463@gmail.com>
2024-01-22 10:29:26 -0600

    Add images
    Each 'slide' container will have it's own pic. The user will be able to rotate through
    these pics, as if it were a slideshow

--- src/userInterface.js
+++ src/userInterface.js
@@ -3,11 +3,11 @@ import { slidesArray } from "./index";
 // eslint-disable-next-line import/prefer-default-export
 export const domController = function () {
 
-    function generateSlide(slide) {
+    function renderSlide(slide) {
         console.log(slide);
     }
 
 
 
-    return { generateSlide}
+    return { renderSlide }
 }
