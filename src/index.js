d405314c6b42080be258154d9e1139275e61b6d9
tgorsk15 <tgbusiness463@gmail.com>
2024-01-22 10:00:18 -0600

    Add userInterface.js and buttonLogic.js,  rerun ESL and Prettier files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import './styles.css';
+import "./styles.css";
+
+// console.log("Why are we still here?");
+// console.log("Yes");
+
+// function templateStarter() {
+//   console.log("is Prettier working?");
+// }
+
+// function newList() {
+//   console.log("yay");
+// }
+
+export const slidesArray = [];
+
+const slides = document.querySelectorAll(".slide");
+
+slides.forEach(slide => {
+  slidesArray.push(slide);
+
+});
+console.log(slidesArray);
 
 
-console.log("Why are we still here?");
-console.log("Yes");
-
-function templateStarter() {
-  console.log("is Prettier working?");
-}
-
-function newList() {
-  console.log("yay");
-}
---
+++ src/userInterface.js
@@ -0,0 +1,13 @@
+import { slidesArray } from "./index";
+
+// eslint-disable-next-line import/prefer-default-export
+export const domController = function () {
+
+    function generateSlide(slide) {
+        console.log(slide);
+    }
+
+
+
+    return { generateSlide}
+}
